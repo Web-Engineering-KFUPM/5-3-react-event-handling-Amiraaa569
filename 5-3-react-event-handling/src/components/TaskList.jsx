63b407acd357bcb4010f020ed87835dab1cf5176
amira <abidoamira@gmail.com>
2025-10-06 11:34:44 +0300

    displaying placeholder if needed and mapping tasks to task items

--- 5-3-react-event-handling/src/components/TaskList.jsx
+++ 5-3-react-event-handling/src/components/TaskList.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import TaskItem from "./TaskItem";
 
 export default function TaskList({ tasks, onDelete }) {
-  return (
-    <ul className="list">
-      {/* Task 2 – Display Placeholder if No Tasks Yet */}
+    return (
+        <ul className="list">
+            {/* Task 2 – Display Placeholder if No Tasks Yet */}
+            {tasks.length === 0 && <li className="placeholder">No tasks yet. Add one above.</li>}
 
-      {/* Task 2 & 3 – Map tasks to TaskItem */}
-      
-    </ul>
-  );
+            {/* Task 2 & 3 – Map tasks to TaskItem */}
+            {tasks.map((task) => (
+                <TaskItem key={task.id} id={task.id} text={task.text} onDelete={onDelete} />
+            ))}
+        </ul>
+    );
 }
+
